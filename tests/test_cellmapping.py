c2142a6959290eb0125ebe34023214341ff1e99c
Chen Yang <healthonrails@gmail.com>
2022-07-06 10:00:46 -0400

    Add test for filter_dog function

--- tests/test_cellmapping.py
+++ tests/test_cellmapping.py
@@ -9,16 +9,20 @@ from cellmapping import cellmapping
 
 
 @pytest.fixture
-def response():
+def galaxy_image():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    import skimage
+    from skimage.color import rgb2gray
+    image = skimage.data.hubble_deep_field()[0:500, 0:500]
+    image_gray = rgb2gray(image)
+    return image_gray
 
 
-def test_content(response):
+def test_filter_dog(galaxy_image):
     """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    from skimage.feature import blob_dog
+    blobs = blob_dog(galaxy_image, min_sigma=1, max_sigma=5, threshold=.1)
+    assert len(blobs) > 0
