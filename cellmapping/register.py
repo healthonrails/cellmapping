4458dabfbfdab2511d274ce998fb96219a6211e7
Chen Yang <healthonrails@gmail.com>
2023-01-20 12:56:30 -0500

    Update paramtermap for registration

--- cellmapping/register.py
+++ cellmapping/register.py
@@ -23,28 +23,18 @@ def register_brain(half_brain_path, atlas_path):
     # Load the Allen Brain Atlas
     allen_brain_atlas = sitk.ReadImage(atlas_path)
 
-    # Set the parameters for the registration process
-    parameter_map = {
-        "MaximumNumberOfIterations": [100],
-        "Metric": ["AdvancedMattesMutualInformation"],
-        "Metric0Weight": [1.0],
-        "Metric1Weight": [0.0],
-        "Metric2Weight": [0.0],
-        "Transform": ["AffineTransform"],
-        "Transform0Weight": [1.0],
-        "Transform1Weight": [0.0],
-        "Transform2Weight": [0.0]
-    }
-
+    parameterMapVector = sitk.VectorOfParameterMap()
+    parameterMapVector.append(sitk.GetDefaultParameterMap("affine"))
+    parameterMapVector.append(sitk.GetDefaultParameterMap("bspline"))
     # Create an ElastixImageFilter object
     registration_method = sitk.ElastixImageFilter()
 
     # Set the fixed and moving images
-    registration_method.SetFixedImage(allen_brain_atlas)
-    registration_method.SetMovingImage(half_mouse_brain)
+    registration_method.SetFixedImage(half_mouse_brain)
+    registration_method.SetMovingImage(allen_brain_atlas)
 
     # Set the parameter map
-    registration_method.SetParameterMap(parameter_map)
+    registration_method.SetParameterMap(parameterMapVector)
 
     # Execute the registration
     registration_method.Execute()
